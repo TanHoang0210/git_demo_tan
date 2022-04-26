3df83d1ab65ab1cc2caf15404317ee773efdf81c
your name <abc@gmail.com>
2022-04-26 22:55:11 +0700

    check it

---
+++ src/javacar/JavaCar.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javacar;
+
+/**
+ *
+ * @author NHC
+ */
+public class JavaCar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
+
+class Car{
+    private String color;
+    private 
+};
